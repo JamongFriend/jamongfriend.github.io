4c9215fc87876199f59d8e253df7df88c649f8b2
JamogFrinds <ghrb6769@gmail.com>
2026-05-11 13:09:05 +0900

    refactor: 헤더 내용 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header>
       <h1>이호규 포트폴리오</h1>
-      <p>Backend & Android Developer | Spring Boot, Node.js, Kotlin, MySQL</p>
+      <p>Backend & Full-Stack Developer | Spring Boot, Node.js, React.js, Android, AWS</p>
     </header>
   );
 };
